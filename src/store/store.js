5f5f4e41760a7d7177bf97557ec735ec83c83318
identityapproved <identityapproved@gmail.com>
2022-07-16 22:23:13 +0300

    connect redux_devtools_extxtension

--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,8 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
-
-const logger = (store) => (next) => (action) => {
-	if (!action.type) next(action)
-
-	console.log('type: ', action.type)
-	console.log('payload: ', action.payload)
-	console.log('currentState: ', store.getState())
-
-	next(action);
-
-	console.log('next state: ', store.getState())
-}
 
 const persistConfig = {
 	key: 'root',
@@ -24,9 +12,10 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const middleWares = [logger]
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean)
 
-const composedEnhancers = compose(applyMiddleware(...middleWares))
+const composedEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares))
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers)
 

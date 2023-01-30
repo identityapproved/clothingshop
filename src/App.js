02097f7c74c2d4610e299c32db0dee634007ab2e
identityapproved <identityapproved@gmail.com>
2023-01-30 15:23:24 +0200

    react suspense and lazy loading

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-import Authentication from './Routes/Authentication/Authentication';
-import CheckoutPage from './Routes/CheckoutPage/CheckoutPage';
-import HomePage from './Routes/HomePage/HomePage';
-import Navigation from './Routes/Navigation/Navigation';
-import ShopPage from './Routes/ShopPage/ShopPage';
-import { checkUserSession } from './store/user/user.action';
+import { lazy, Suspense, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./Routes/Navigation/Navigation";
+import { checkUserSession } from "./store/user/user.action";
+
+const HomePage = lazy(() => import("./Routes/HomePage/HomePage"));
+const Authentication = lazy(() =>
+  import("./Routes/Authentication/Authentication")
+);
+const CheckoutPage = lazy(() => import("./Routes/CheckoutPage/CheckoutPage"));
+const ShopPage = lazy(() => import("./Routes/ShopPage/ShopPage"));
 
 const App = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(checkUserSession())
+    dispatch(checkUserSession());
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<HomePage />} />
-        <Route path='shop/*' element={<ShopPage />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<CheckoutPage />} />
-      </Route>
-    </Routes>
+    <Suspense fallback="<Spinner/>">
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<HomePage />} />
+          <Route path="shop/*" element={<ShopPage />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<CheckoutPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
